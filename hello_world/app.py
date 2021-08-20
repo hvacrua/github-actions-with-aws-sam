dca707a8cf9c22ee0444e98b385d19b2a6462972
hvacrua <kras_pp@ukr.net>
2021-08-20 18:20:22 +0300

    change greeting

--- hello_world/app.py
+++ hello_world/app.py
@@ -33,11 +33,8 @@ def lambda_handler(event, context):
 
     #     raise e
 
-    return "Hello AWS!"
-    # return {
-    #     "statusCode": 200,
-    #     "body": json.dumps({
-    #         "message": "hello world",
-    #         # "location": ip.text.replace("\n", "")
-    #     }),
-    # }
+    return {"statusCode": 200,
+            "body": json.dumps({"message": "Hello AWS!",
+        # "location": ip.text.replace("\n", "")
+                                }),
+            }
